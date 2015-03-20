1f08f8777468f6b1ad51f2b7c892d768beb0b72c
Petrashko Evgeny <petrashkoev@gmail.com>
2015-03-20 17:40:04 +0300

    ios project

---
+++ Xamarin Project/SelfEducation/SelfEducation/SelfEducation.iOS/RootViewController.designer.cs
@@ -0,0 +1,45 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace SelfEducation.iOS
+{
+	[Register ("RootViewController")]
+	partial class RootViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton LogInButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField logInText { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField PasswordText { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (LogInButton != null) {
+				LogInButton.Dispose ();
+				LogInButton = null;
+			}
+			if (logInText != null) {
+				logInText.Dispose ();
+				logInText = null;
+			}
+			if (PasswordText != null) {
+				PasswordText.Dispose ();
+				PasswordText = null;
+			}
+		}
+	}
+}
