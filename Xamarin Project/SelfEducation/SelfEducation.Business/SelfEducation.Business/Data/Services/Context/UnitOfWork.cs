8b5d968b9d07eaa8ba84913ea798deda0667d4d3
Petrashko Evgeny <petrashkoev@gmail.com>
2015-03-31 16:10:11 +0300

    Update unit of work

---
+++ Xamarin Project/SelfEducation/SelfEducation.Business/SelfEducation.Business/Data/Services/Context/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using SelfEducation.Business.Data.Entities;
+using SelfEducation.Business.Data.Repository.Private;
+using SelfEducation.Business.Data.Services.Context.Private;
+using SelfEducation.Business.Data.Services.Transaction.Private;
+
+namespace SelfEducation.Business.Data.Services.Context
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(ICreateTablesTransaction createTablesTransaction, ICreateDefaultTagsTransaction createDefaultTagsTransaction)
+        {
+            createTablesTransaction.Execute();
+            createDefaultTagsTransaction.Execute();
+        }
+
+        public IConnectionExtensions Extension
+        {
+            get { return ServiceLocator.Current.GetInstance<IConnectionExtensions>(); }
+        }
+
+        public IRepository<CurrencyEntity> CurrencyRepository
+        {
+            get { return ServiceLocator.Current.GetInstance<IRepository<CurrencyEntity>>(); }
+        }
+    }
+}
